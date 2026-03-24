3638fd7b5c9505a24d73a1aab45188bcb5e9c3ef
Joao Oliveira <jhcardosodeoliveira@gmail.com>
2026-03-24 21:30:14 +0000

    RabbitMQ channels created

--- RabbitMQ/RabbitMQBus.cs
+++ RabbitMQ/RabbitMQBus.cs
@@ -1,26 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
+using RabbitMQ.Client;
 using Microsoft.Extensions.Logging;
 
 namespace RabbitMQ
 {
-	public interface IBusPublisher { Task Publish<T>(T message); }
+	public interface IBusPublisher
+	{
+		Task Publish<T>(T message, string? routingKey = null) where T : class;
+	}
 
 	public class RabbitMQBus : IBusPublisher
 	{
-		private readonly IModel _channel;
-		public RabbitMQBus(IConnection connection) => _channel = connection.CreateModel();
+		private readonly IRabbitMQConnection _connection;
+		private readonly ILogger<RabbitMQBus> _logger;
 
-		public Task Publish<T>(T message)
+		public RabbitMQBus(IRabbitMQConnection connection, ILogger<RabbitMQBus> logger)
 		{
+			_connection = connection;
+			_logger = logger;
+		}
+
+		public Task Publish<T>(T message, string? routingKey = null) where T : class
+		{
+			using var channel = _connection.CreateModel();
+			channel.ExchangeDeclare(exchange: "order_exchange", type: ExchangeType.Topic, durable: true);
+
+			var messageName = routingKey ?? typeof(T).Name;
 			var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
-			_channel.BasicPublish(exchange: "order_exchange", routingKey: typeof(T).Name, body: body);
+
+			channel.BasicPublish(
+				exchange: "order_exchange",
+				routingKey: messageName,
+				basicProperties: null,
+				body: body);
+
+			_logger.LogInformation("Published {MessageName} to RabbitMQ", messageName);
 			return Task.CompletedTask;
 		}
 	}
-
 }
